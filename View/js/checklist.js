a04ea66c6834e4d64106169c1880d5fa9764752f
frezmecritus <frezmecritus@gmail.com>
2015-11-01 01:11:35 -0500

    checklist toggle and fadeIn fadeOut animation

--- View/js/checklist.js
+++ View/js/checklist.js
@@ -4,4 +4,12 @@
  * and open the template in the editor.
  */
 
-
+$(document).on("click", ".word", function(){
+    var list = $(this).parent().siblings();
+    if($(list).css('opacity') == '1') {
+        $(list).animate({opacity:0}, 800, function(){$(this).toggle(800);});
+    }
+    else {
+        $(list).toggle(800).animate({opacity:1});
+    }
+});
